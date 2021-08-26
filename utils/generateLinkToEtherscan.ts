84b708bb670ee360f896563d41d0f1c39f2427fa
MrKampla <mrkampla@gmail.com>
2021-08-27 01:30:44 +0200

    Fix blockchain explorer lnk generation

--- utils/generateLinkToEtherscan.ts
+++ utils/generateLinkToEtherscan.ts
@@ -2,7 +2,7 @@ import { NETWORK_NAMES } from '../constants/networks';
 
 const generateLinkToBlockchainExplorer = (data: string, type: string, networkId: number) => {
   const protocol = 'https://';
-  const baseUrl = `etherscan.io/${type}/${data}`;
+  const baseUrl = `.etherscan.io/${type}/${data}`;
   const networkName = NETWORK_NAMES[networkId as keyof typeof NETWORK_NAMES];
   if (!networkName || networkName === 'mainnet') {
     return `${protocol}${baseUrl}`;
